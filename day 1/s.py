1ddb40b0db4b30a1b927397078614f5be61ffc19
gruenhoschi <9988808+gruenhoschi@users.noreply.github.com>
2022-12-12 14:26:12 +0100

    day 1 done

--- day 1/s.py
+++ day 1/s.py
@@ -1,15 +1,17 @@
 f = open("input.txt")
 sum = 0
-max = 0
-
+calories_list = list()
 for i in f:
     if i.strip() == "":
-        print("sum", sum)
-        if sum > max: max = sum
+        calories_list.append(sum)
         sum = 0
     else:
         sum += int(i.strip())
 
-print("max", max)
+calories_list.sort(reverse = True)
 
+sum = 0
+for i in range(3):
+    sum += calories_list[i]
 
+print("sum",sum)
