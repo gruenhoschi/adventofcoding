b32eb959380cfdc341723c4a2c43ddaac50dca2b
gruenhoschi <9988808+gruenhoschi@users.noreply.github.com>
2022-12-12 15:48:26 +0100

    day 2 part 2

--- day 2/s.py
+++ day 2/s.py
@@ -2,7 +2,7 @@ outcome_points = [1,3,6] # 0 => Lose, 3 => draw,  6 => win
 shape_points   = {"X":1,"Y":2,"Z":3} # 1 => Rock, 2 => Paper, 3 => Scissor
 shape_opponent = {"A":1,"B":2,"C":3} # Rock, Paper, Scissor
 win_condition  = {1:3,2:1,3:2}
-
+lose_condition = {3:1,1:2,2:3}
 
 f = open("input.txt")
 
@@ -16,24 +16,12 @@ player = [0,0]
 for i in f:
     s = i.split(" ")
     player[0] = shape_opponent.get(s[0])
-    player[1] = shape_points.get(s[1].strip())
+    expected_outcome = s[1].strip()
+    if expected_outcome == "X": #loose
+        tot_score += win_condition.get(player[0])
+    elif expected_outcome == "Y": #draw
+        tot_score += 3 + player[0]
+    else: #win
+        tot_score += 6 + lose_condition.get(player[0])
 
-    tot_score += player[1]
-
-    if win_condition.get(player[1]) == player[0]:
-        print("me winning",player[1],player[0])
-        tot_score += 6
-        win += 1
-    elif player[0] == player[1]:
-        print("draw")
-        tot_score += 3
-        draw += 1
-    else:
-        print("lost")
-        tot_score += 0
-        lost += 1
-print("win",win)
-print("draw", draw)
-print("lost",lost)
-print(win + draw + lost)
 print(tot_score)
